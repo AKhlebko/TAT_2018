418132b3d10fda868d0a6afd227c8800a1bd4502
lowara1488 <thegreendeers@gmail.com>
2018-03-01 20:35:21 +0300

    Refactored code

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevTask1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            SameSymbolSubtringFinder handler = new SameSymbolSubtringFinder(args);
+            Console.WriteLine(handler.LongestSubStringSameSymbols());
+        }
+    }
+}
---
+++ DEV-1/DEV-1/SameSymbolSubstringFinder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DevTask1
+{
+    class SameSymbolSubstringFinder
+    {
+        /// <summary>
+        /// This class contains methods for getting string
+        /// and for for finding the longest same character substring
+        /// Stores processed string in a private field
+        /// </summary>        
+        private string str;
+
+        public int LongestSubStringSameSymbols()
+        {
+            /// <summary>
+            /// Returns length of the longest substring consisting of the same symbols
+            /// </summary>
+            int MaxStringLength = 0;
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                int tempCounter = 1;
+                int j = i + 1;
+                while (j < str.Length)
+                {
+                    try
+                    {
+                        if (str[i] == str[j])
+                        {
+                            j++;
+                            tempCounter++;
+                        }
+                        else
+                        {
+                            i = j-1;
+                            break;
+                        }
+                    }
+                    catch (IndexOutOfRangeException ex)
+                    {
+
+                    }
+                }
+                if (tempCounter > MaxStringLength)
+                {
+                    MaxStringLength = tempCounter;
+                }
+            }
+            return MaxStringLength;
+        }
+
+        public SameSymbolSubstringFinder(string[] args)
+        {
+            /// <summary>
+            /// StringHandler class's instance initializer
+            /// Gets console args as input arguments
+            /// </summary>        
+            if (args.Length == 0)
+            {
+                str = "";
+            }
+            else
+            {
+                GetStringFromArguments(args);
+            }
+        }
+
+        private void GetStringFromArguments(string[] args)
+        {
+            /// <summary>
+            /// Makes string from the console arguments
+            /// </summary>        
+            str = String.Join("", args);
+            return;
+        }
+    }
+}
