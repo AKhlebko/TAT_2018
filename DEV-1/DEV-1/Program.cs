fd980fbd956261ff07f16c6375d0d8914c9900f1
lowara1488 <thegreendeers@gmail.com>
2018-03-01 19:57:48 +0300

    task DEV-1

---
+++ DEV-1/DEV-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EntryPoint
+{
+
+    class StringHandler
+    {
+        public static int LongestEqualSymbols(string str)
+        {
+            int length = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                Boolean equal = true;
+                int buffer = 0;
+                int j = i;
+                while (equal && j < str.Length)
+                {
+                    if (str[i] == str[j])
+                    {
+                        j++;
+                        buffer++;
+                    }
+                    else
+                    {
+                        equal = false;
+                    }
+                }
+                if (buffer > length) length = buffer;
+            }
+            return length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            Console.WriteLine(StringHandler.LongestEqualSymbols(line));
+        }
+    }
+}
