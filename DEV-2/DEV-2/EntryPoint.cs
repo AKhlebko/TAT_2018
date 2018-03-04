46930266289bd1ede3ccd7a3922a0105f5003a8c
lowara1488 <thegreendeers@gmail.com>
2018-03-04 12:26:15 +0300

    task 2

---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace TaskDEV2
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            OnlyEvenPositionCharsString evener = new OnlyEvenPositionCharsString(str);
+            evener.LeaveOnlyEvenPositionChars();
+        }
+    }
+}
---
+++ DEV-2/DEV-2/OnlyEvenPositionCharsString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace TaskDEV2
+{
+    /// <summary>
+    /// Class contains methods for deleting all odd symbols 
+    /// in the string using StringBilder
+    /// </summary>
+    class OnlyEvenPositionCharsString
+    {
+        StringBuilder processedString;
+
+        /// <summary>
+        /// Initializer for class's instances
+        /// </summary>
+        /// <param name="str">
+        /// StringBuilder parameter for 
+        /// processedString assignment
+        /// </param>
+        public OnlyEvenPositionCharsString(StringBuilder str)
+        {
+            processedString = str;
+        }
+        
+        /// <summary>
+        /// Method for leaves only even positions char
+        /// in the processedString
+        /// </summary>
+        public void LeaveOnlyEvenPositionChars()
+        {
+            if (processedString.Length < 2)
+            {
+                return;
+            }
+            int deletePos = 1;
+            while (deletePos < processedString.Length)
+            {
+                processedString.Remove(deletePos, 1);
+                deletePos++;
+            }
+            Console.WriteLine($"String with only even position chars - {processedString}");
+        }     
+    }
+}
