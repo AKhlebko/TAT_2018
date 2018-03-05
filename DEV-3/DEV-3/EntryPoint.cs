da91b3d76e2342b0bceaabd13652cfeab40b896d
lowara1488 <thegreendeers@gmail.com>
2018-03-05 23:16:12 +0300

    task 3 Anton Khlebko

---
+++ DEV-3/DEV-3/EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace taskDEV3
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            BigInteger decimalNumer;
+            short newRadix;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("ERROR: Wrong number of arguments.");
+            }            
+            else if (!BigInteger.TryParse(args[0], out decimalNumer))
+            {
+                Console.WriteLine("ERROR: Input decimal is not a number");
+            }
+            else if (decimalNumer.Sign == -1)
+            {
+                Console.WriteLine("ERROR: Can't convert negative numbers");
+            }
+            else if (!short.TryParse(args[1], out newRadix))
+            {
+                Console.WriteLine("ERROR: Input radix is not a number");
+            }
+            else if (newRadix > 2 && newRadix < 20)
+            {
+                RadixConvenet converter = new RadixConvenet(decimalNumer, newRadix);
+                converter.ConvertNumberToNewRadix();
+                converter.PrintNumberInNewRadix();
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: I can't conver this number to the {newRadix} radix");
+            }
+        }
+    }
+}
---
+++ DEV-3/DEV-3/RadixChanger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+namespace taskDEV3
+{
+    /// <summary>
+    /// Class which stores decimal number's form
+    /// and can switch it into some other radixs from 2 to 20
+    /// </summary>
+    class RadixConvenet
+    {
+        BigInteger decimalNumber;
+        short newRadix;
+        StringBuilder numberInNewRadix;
+        
+        public RadixConvenet(BigInteger pDecimalNumber, short pNewRadix)
+        {
+            decimalNumber = pDecimalNumber;
+            newRadix = pNewRadix;
+            numberInNewRadix = new StringBuilder();
+        }
+
+        /// <summary>
+        /// Method converts decimal number into a number with another radix
+        /// I count it with charCode = Code('A') + modulo - 10 = 
+        /// = 55 + modulo;
+        /// </summary>
+        public void ConvertNumberToNewRadix()
+        {
+            BigInteger bufferDecimalNumber = decimalNumber;
+            while (bufferDecimalNumber > 0)
+            {
+                short modulo = (short)(bufferDecimalNumber % newRadix);
+                if (modulo < 10)
+                {
+                    numberInNewRadix.Insert(0, modulo);
+                }
+                else
+                {
+                    char newRadixDigit = (char)(55 + modulo);
+                    numberInNewRadix.Insert(0, newRadixDigit);
+                }
+                bufferDecimalNumber /= newRadix;
+            }
+        }
+        
+        /// <summary>
+        /// Prints the number in the new Radix
+        /// </summary>            
+        public void PrintNumberInNewRadix()
+        {
+            Console.WriteLine($"Number {decimalNumber} in radix {newRadix} is {numberInNewRadix}");
+        } 
+    }
+}
